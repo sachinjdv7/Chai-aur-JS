4a54b741d7dffb039ae4bea5c1086f7ee9adf1eb
sachin jadhav <sachin.jadhav@tridiagonal.com>
2024-02-14 21:52:31 +0530

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"user strict"; // treat all JS code as newer version
+
+// alert(3 + 3);
+/**
+ * We are using node js not a browser
+ */
+
+// Data Types
+
+let name = "sachin";
+let age = 18;
+isAlive = true;
+
+//number
+//string
+//bigint (if we have big number then we can use it)
+//Boolean (yes or no or 1 or 0)
+//null   (it is standalone value in js)
+//undefined (value is not assigned yet)
+//symbol (useful to find uniqueness)
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // null
