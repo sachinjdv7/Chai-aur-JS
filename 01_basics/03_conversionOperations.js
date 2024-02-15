a8cd2e675db6af24b4375a0c4eac455906fefda0
sachin jadhav <sachin.jadhav@tridiagonal.com>
2024-02-15 21:16:39 +0530

    conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,27 @@
+let score = "sachin";
+
+// console.log(typeof score);
+// console.log(typeof score);
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// '33' => number
+// '333abs' => NaN
+// true => 1  false => 0
+
+let isLoggedIn = "sachin";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1 => true 0 => false
+// '' => false
+// 'sachin' => true
+
+let someNumber = 55;
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
