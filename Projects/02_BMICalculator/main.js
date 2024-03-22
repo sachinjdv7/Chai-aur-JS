a951c3369ab6d068439c7f73666b1c9afa80033c
sachin jadhav <sachin.jadhav@tridiagonal.com>
2024-03-22 17:28:18 +0530

    project 2 BMI Calculator

---
+++ Projects/02_BMICalculator/main.js
@@ -0,0 +1,20 @@
+const form = document.querySelector("form");
+// This use case will give you empty value
+// const height = parseInt(document.querySelector("#height").value);
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const height = parseInt(document.querySelector("#height").value);
+  const weight = parseInt(document.querySelector("#weight").value);
+  const results = document.querySelector("#results");
+
+  if (height === "" || height < 0 || isNaN(height)) {
+    results.innerHTML = `Please give a valid ${height}`;
+  } else if (weight === "" || weight < 0 || isNaN(weight)) {
+    results.innerHTML = `Please give a valid ${weight}`;
+  } else {
+    const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+    // show the result
+    results.innerHTML = `<span>${bmi}</span>`;
+  }
+});
