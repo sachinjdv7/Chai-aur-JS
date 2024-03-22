cf32abf69467af93bce4840c9cd991f56c187e3a
sachin jadhav <sachin.jadhav@tridiagonal.com>
2024-03-22 16:43:48 +0530

    color switcher project 1 completed

---
+++ Projects/Color_Switcher/main.js
@@ -0,0 +1,26 @@
+const buttons = document.querySelectorAll(".button");
+const body = document.querySelector("body");
+console.log(buttons);
+
+buttons.forEach((button) => {
+  //   console.log(button);
+  button.addEventListener("click", function (e) {
+    console.log(e);
+    console.log(e.target.id);
+    if (e.target.id === "grey") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "white") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "blue") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "yellow") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "goldenrod") {
+      body.style.backgroundColor = e.target.id;
+    }
+  });
+});
