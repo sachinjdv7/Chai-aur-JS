6ded36ee58e6af4c46e1ffb94b1258ddb73c131c
sachin jadhav <sachin.jadhav@tridiagonal.com>
2024-04-09 08:17:03 +0530

    learn inheritance using es6

---
+++ 10_classes_and_oop/inheritance.js
@@ -0,0 +1,27 @@
+class User {
+  constructor(username) {
+    this.username = username;
+  }
+  logMe() {
+    console.log(`User Name is ${this.username}`);
+  }
+}
+
+class Teacher extends User {
+  constructor(username, email, password) {
+    super(username);
+    this.email = email;
+    this.password = password;
+  }
+  addCourse() {
+    console.log(`A new course added by ${this.username}`);
+  }
+}
+
+const chai = new Teacher("chai", "chai@gmail.com", "123");
+chai.logMe();
+
+const masalaChai = new User("masalachai");
+masalaChai.logMe();
+
+console.log(chai instanceof User);
