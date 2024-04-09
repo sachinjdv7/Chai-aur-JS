36ca55f29644cb0eb27dd4b0643e98f9e962595b
sachin jadhav <sachin.jadhav@tridiagonal.com>
2024-04-09 04:55:47 -0500

    mathpi story learn

---
+++ 10_classes_and_oop/mathpi.js
@@ -0,0 +1,25 @@
+// const descriptor = Object.getOwnPropertyDescriptor(Math, "PI");
+
+// console.log(descriptor);
+
+const chai = {
+  name: "ginger chai",
+  price: 250,
+  isAvailable: true,
+  orderChai: function () {
+    console.log("Chai ordered");
+  },
+};
+
+Object.defineProperty(chai, "name", {
+  //   writable: false,
+  enumerable: false,
+  //   configurable: false,
+});
+// console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+
+for (let [key, value] of Object.entries(chai)) {
+  if (typeof value !== "function") {
+    console.log(`${key}, ${value}`);
+  }
+}
